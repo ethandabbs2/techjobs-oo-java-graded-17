7fa21b1fe346ae71f34eed9741b7cc238825f98c
ethandabbs2 <ethandabbs2@yahoo.com>
2024-01-18 14:48:08 -0600

    commit 2 OS-agnostic version

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -48,33 +48,37 @@ public class JobTest {
                 new CoreCompetency(""));
         String firstChar = String.valueOf(acmeProductTester.toString().charAt(0));
         String lastChar = String.valueOf(acmeProductTester.toString().charAt(acmeProductTester.toString().length()-1));
-        assertEquals(firstChar, lineSeparator());
-        assertTrue(acmeProductTester.toString().startsWith(System.lineSeparator()));
-        assertEquals(lastChar, lineSeparator());
+        assertTrue(acmeProductTester.toString().startsWith(lineSeparator()));
+        //assertEquals(firstChar, lineSeparator());
+        //assertTrue(acmeProductTester.toString().startsWith(System.lineSeparator()));
+        //assertEquals(lastChar, lineSeparator());
         assertTrue(acmeProductTester.toString().endsWith(System.lineSeparator()));
     }
 
     @Test
     public void testToStringContainsCorrectLabelsAndData() {
-        String expectedString = lineSeparator() + "ID: 4" + lineSeparator() +
-                "Name: Product tester" + lineSeparator() + "Employer: ACME" + lineSeparator() +
-                "Location: Desert" + lineSeparator() + "Position Type: Quality control" +
-                lineSeparator() + "Core Competency: Persistence" + lineSeparator();
+
         Job acmeProductTester = new Job("Product tester", new Employer("ACME"),
                 new Location("Desert"), new PositionType("Quality control"),
                 new CoreCompetency("Persistence"));
+        String expectedString = lineSeparator() + "ID: " + acmeProductTester.getId() + lineSeparator() +
+                "Name: Product tester" + lineSeparator() + "Employer: ACME" + lineSeparator() +
+                "Location: Desert" + lineSeparator() + "Position Type: Quality control" +
+                lineSeparator() + "Core Competency: Persistence" + lineSeparator();
         assertEquals(expectedString, acmeProductTester.toString());
+
     }
 
     @Test
     public void testToStringHandlesEmptyField() {
-        String expectedString = lineSeparator() + "ID: 3" + lineSeparator() +
-                "Name: Product tester" + lineSeparator() + "Employer: Data not available" + lineSeparator() +
-                "Location: Desert" + lineSeparator() + "Position Type: Data not available" +
-                lineSeparator() + "Core Competency: Data not available" + lineSeparator();
+
         Job acmeProductTester = new Job("Product tester", new Employer(""),
                 new Location("Desert"), new PositionType(""),
                 new CoreCompetency(""));
+        String expectedString = lineSeparator() + "ID: " + acmeProductTester.getId() + lineSeparator() +
+                "Name: Product tester" + lineSeparator() + "Employer: Data not available" + lineSeparator() +
+                "Location: Desert" + lineSeparator() + "Position Type: Data not available" +
+                lineSeparator() + "Core Competency: Data not available" + lineSeparator();
         assertEquals(expectedString, acmeProductTester.toString());
 
     }
